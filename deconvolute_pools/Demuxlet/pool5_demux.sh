dbb39b56907df3c27c6e7e2704d05f0f48e4f15a
Maggie Brown <36750960+maggiebr0wn@users.noreply.github.com>
2023-07-21 15:54:11 -0400

    Create pool5_demux.sh

---
+++ deconvolute_pools/Demuxlet/pool5_demux.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+plink_vcf="/storage/home/mfisher42/scProjects/CD_Subra/rawdata/Genotype_Data/Genos_by_Pool/Pool5/LiftOver_to_Hg38/hg38_output.vcf"
+
+# in dir with file, fix order of vcf chromosome to be lexicographically
+cat $plink_vcf | awk '$1 ~ /^#/ {print $0;next} {print $0 | "sort -k1,1 -k2,2n"}' > sorted_vcf.vcf
+
+# run Demuxlet
+demuxlet --sam sorted_bam.bam --tag-group CB --tag-UMI UB --vcf sorted_vcf.vcf --field GT --out pool5_demux
